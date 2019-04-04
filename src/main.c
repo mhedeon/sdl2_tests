9f5362b44da9f4f7f6e4a6ae6f39d0cb9fbcbe2e
Maks <maksged@gmail.com>
2019-04-04 21:55:26 +0300

    text input fixed, text render fixed

--- src/main.c
+++ src/main.c
@@ -2,14 +2,89 @@
 
 void init(t_test *test)
 {
-	test->win = create_win("test", 800, 600);
-	test->font = ttf_open_font("./libraries/libmgl/ttf/OSR.ttf", 20);
+	test->win = create_win("test", 2000, 600);
+	test->font = ttf_open_font("./libraries/libmgl/ttf/OpenSans-Regular.ttf", 30);
+}
+
+void			ttf_render_solid(SDL_Renderer *ren, TTF_Font *font, SDL_Rect *r,
+															char *text)
+{
+	SDL_Surface	*sur;
+	SDL_Texture	*msg;
+
+	sur = TTF_RenderText_Solid(font, text, (SDL_Color) { 255, 255, 255, 0 });
+	if (sur == NULL)
+	{
+		error_log("Text error");
+		return ;
+	}
+	msg = SDL_CreateTextureFromSurface(ren, sur);
+	if (msg == NULL)
+	{
+		SDL_FreeSurface(sur);
+		error_log("Text error");
+		return ;
+	}
+	SDL_RenderCopy(ren, msg, NULL, r);
+	SDL_FreeSurface(sur);
+	SDL_DestroyTexture(msg);
+}
+
+void			ttf_render_blended(SDL_Renderer *ren, TTF_Font *font, SDL_Rect *r,
+															char *text)
+{
+	SDL_Surface	*sur;
+	SDL_Texture	*msg;
+
+	sur = TTF_RenderText_Blended(font, text, (SDL_Color) { 255, 255, 255, 0 });
+	if (sur == NULL)
+	{
+		error_log("Text error");
+		return ;
+	}
+	msg = SDL_CreateTextureFromSurface(ren, sur);
+	if (msg == NULL)
+	{
+		SDL_FreeSurface(sur);
+		error_log("Text error");
+		return ;
+	}
+	SDL_RenderCopy(ren, msg, NULL, r);
+	SDL_FreeSurface(sur);
+	SDL_DestroyTexture(msg);
+}
+
+void			ttf_render_shaded(SDL_Renderer *ren, TTF_Font *font, SDL_Rect *r,
+															char *text)
+{
+	SDL_Surface	*sur;
+	SDL_Texture	*msg;
+
+	sur = TTF_RenderText_Shaded(font, text, (SDL_Color) { 255, 255, 255, 0 }, (SDL_Color) { 255, 0, 0, 0 });
+	if (sur == NULL)
+	{
+		error_log("Text error");
+		return ;
+	}
+	msg = SDL_CreateTextureFromSurface(ren, sur);
+	if (msg == NULL)
+	{
+		SDL_FreeSurface(sur);
+		error_log("Text error");
+		return ;
+	}
+	SDL_RenderCopy(ren, msg, NULL, r);
+	SDL_FreeSurface(sur);
+	SDL_DestroyTexture(msg);
 }
 
 int main()
 {
 	t_test	*test;
-	char text[32];
+	// char text[32];
+	char *text = (char*)malloc(sizeof(char));
+							text[0] = '\0';
+	int len = 1;
 	char *composition;
 	Sint32 cursor;
 	Sint32 selection_len;
@@ -25,8 +100,15 @@ int main()
 		SDL_Event e;
 		if (SDL_PollEvent(&e))
 		{
+			if (KEY == SDLK_BACKSPACE)
+				text[strlen(text) - 1] = '\0';
 			if (KEY == SDLK_ESCAPE)
 				done = SDL_TRUE;
+			if (KEY == SDLK_RETURN)
+			{
+				text = realloc(text, --len);
+				strncat(text, "\n", len - strlen(text) - 1);
+			}
 			switch (e.type)
 			{
 				case SDL_QUIT:
@@ -35,8 +117,9 @@ int main()
 					break;
 				case SDL_TEXTINPUT:
 					/* Add new text onto the end of our text */
-					strncat(text, e.text.text, 32 - strlen(text) - 1);
-					// printf("text: %s\n\n", e.text.text);
+					text = realloc(text, ++len);
+					strncat(text, e.text.text, len - strlen(text) - 1);
+					// strncat(text, e.text.text, 32 - strlen(text) - 1);
 					printf("inpt\n");
 					break;
 				case SDL_TEXTEDITING:
@@ -53,11 +136,22 @@ int main()
 					break;
 			}
 		}
-		printf("text: %s\n\n", text);
-		// ttf_render_text(test->win->ren, test->font, &(SDL_Rect) { 50, 50, selection_len, 20 }, text);
-		upd_win(test->win);
-	}
-	
+		// printf("text: %s\n\n", text);
 
+		SDL_UpdateTexture(test->win->tex, NULL, test->win->buff,
+			test->win->w * sizeof(Uint32));
+		SDL_RenderClear(test->win->ren);
+		SDL_RenderCopy(test->win->ren, test->win->tex, NULL, NULL);
+
+		int w, h;
+		TTF_SizeText(test->font, text, &w, &h);
+		ttf_render_solid(test->win->ren, test->font, &(SDL_Rect) { 50, 50, w, h }, text);
+		ttf_render_blended(test->win->ren, test->font, &(SDL_Rect) { 50, 150, w, h }, text);
+		ttf_render_shaded(test->win->ren, test->font, &(SDL_Rect) { 50, 250, w, h }, text);
+
+		SDL_RenderPresent(test->win->ren);
+	}
+
+	// system("leaks test");
 	return (0);
 }
